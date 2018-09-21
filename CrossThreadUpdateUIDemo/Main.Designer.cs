033e80ccbb12544a23ca2f149ca1c69a27e36b44
RoyLiu <121407741@qq.com>
2018-09-21 15:26:21 +0800

    init commit

---
+++ CrossThreadUpdateUIDemo/Main.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CrossThreadUpdateUIDemo
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvoke = new System.Windows.Forms.Button();
+            this.txtTime1 = new System.Windows.Forms.TextBox();
+            this.btnSyncContext = new System.Windows.Forms.Button();
+            this.txtTime2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInvoke
+            // 
+            this.btnInvoke.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInvoke.Location = new System.Drawing.Point(6, 65);
+            this.btnInvoke.Name = "btnInvoke";
+            this.btnInvoke.Size = new System.Drawing.Size(481, 23);
+            this.btnInvoke.TabIndex = 0;
+            this.btnInvoke.Text = "TestInvoke";
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_Click);
+            // 
+            // txtTime1
+            // 
+            this.txtTime1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTime1.Location = new System.Drawing.Point(6, 21);
+            this.txtTime1.Name = "txtTime1";
+            this.txtTime1.ReadOnly = true;
+            this.txtTime1.Size = new System.Drawing.Size(481, 22);
+            this.txtTime1.TabIndex = 1;
+            // 
+            // btnSyncContext
+            // 
+            this.btnSyncContext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSyncContext.Location = new System.Drawing.Point(6, 65);
+            this.btnSyncContext.Name = "btnSyncContext";
+            this.btnSyncContext.Size = new System.Drawing.Size(481, 23);
+            this.btnSyncContext.TabIndex = 2;
+            this.btnSyncContext.Text = "TestSynchronizationContext";
+            this.btnSyncContext.UseVisualStyleBackColor = true;
+            this.btnSyncContext.Click += new System.EventHandler(this.btnSyncContext_Click);
+            // 
+            // txtTime2
+            // 
+            this.txtTime2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTime2.Location = new System.Drawing.Point(6, 21);
+            this.txtTime2.Name = "txtTime2";
+            this.txtTime2.ReadOnly = true;
+            this.txtTime2.Size = new System.Drawing.Size(481, 22);
+            this.txtTime2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtTime1);
+            this.groupBox1.Controls.Add(this.btnInvoke);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 94);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test Invoke";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtTime2);
+            this.groupBox2.Controls.Add(this.btnSyncContext);
+            this.groupBox2.Location = new System.Drawing.Point(12, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(493, 94);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Test Synchronization";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 243);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInvoke;
+        private System.Windows.Forms.TextBox txtTime1;
+        private System.Windows.Forms.Button btnSyncContext;
+        private System.Windows.Forms.TextBox txtTime2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ CrossThreadUpdateUIDemo/Main.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DateTime = System.DateTime;
+
+namespace CrossThreadUpdateUIDemo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvoke_Click(object sender, EventArgs e)
+        {
+            var thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Action action = () => txtTime1.Text = DateTime.Now.ToLongTimeString();
+
+                    if (InvokeRequired && !Disposing)
+                    {
+                        Invoke(action);
+                    }
+                    else
+                    {
+                        action();
+                    }
+
+                    Task.Delay(1000);
+                }
+            }) {IsBackground = true};
+
+            thread.Start();
+        }
+
+        private void btnSyncContext_Click(object sender, EventArgs e)
+        {
+            var uiContext = SynchronizationContext.Current;
+
+            var thread = new Thread(ctx =>
+            {
+                var context = ctx as SynchronizationContext;
+                if (context == null)
+                {
+                    return;
+                }
+
+                while (true)
+                {
+                    context.Send(state =>
+                    {
+                        var dateString = state as string;
+                        if (string.IsNullOrEmpty(dateString))
+                        {
+                            return;
+                        }
+
+                        txtTime2.Text = dateString;
+                    }, DateTime.Now.ToLongTimeString());
+                }
+            }) {IsBackground = true};
+
+            thread.Start(uiContext);
+        }
+    }
+}
